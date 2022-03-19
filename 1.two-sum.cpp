38f0182c2c3d05110dfa36ea9fbcb352323b84eb
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-03-19 10:15:18 +0530

    two sum

---
+++ 1.two-sum.cpp
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        vector<int>res;
+        for(int i = 0;i<nums.size();i++){
+            for(int j = i+1;j<nums.size();j++){
+                if(nums[i]+nums[j] == target) {
+                    res.push_back(i);
+                    res.push_back(j);
+                }
+
+            }
+        }
+        return res;
+    }
+};
+// @lc code=end
