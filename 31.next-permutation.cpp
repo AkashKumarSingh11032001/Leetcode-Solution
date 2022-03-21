8fa0807e25d61776d23253284ccf234ea0c5f71f
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-03-21 22:40:35 +0530

    Next Permutation

---
+++ 31.next-permutation.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=31 lang=cpp
+ *
+ * [31] Next Permutation
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        next_permutation(nums.begin(), nums.end());
+
+        
+    }
+};
+// @lc code=end
+
